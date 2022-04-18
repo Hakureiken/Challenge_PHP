ab4e6928ca1eb9184755067a637bb848323dac85
kevin gaste <kevin.gaste10@gmail.com>
2022-04-18 21:37:30 +0200

    change

--- index.php
+++ index.php
@@ -10,68 +10,65 @@ include 'arrayTeams.php';
 // - La photo du stade
 ?>
 
-<nav>
+<!DOCTYPE html>
+<html lang="fr">
 
-</nav>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Football</title>
+</head>
 
-<main>
-    <?php
-    foreach ($premierLeagueTeams as $key => $value) {
+<body>
+    <nav>
+        <!-- mettre une navbar ici -->
+    </nav>
 
-        foreach ($value as $team => $informationTeam) {
-    ?>
-            <a class="changeToDetails" href="details.php?id=<?= (int)$team; ?>">
-                <div class="container" id="">
-                    <div class="card">
-                        <div class="teams">
-                            <h2><?= $informationTeam['strAlternate']; ?></h2> <!-- nom de l'équipe --->
-                            <img class="imgEcusson" src="<?= $informationTeam['strTeamBadge']; ?>" alt=""> <!-- écusson --->
+    <main>
+        <?php
+        foreach ($premierLeagueTeams as $key => $value) {
+
+            foreach ($value as $team => $informationTeam) {
+        ?>
+                <a class="changeToDetails" href="details.php?id=<?= (int)$team; ?>">
+                    <div class="container" id="">
+                        <div class="card">
+                            <div class="teams">
+                                <h2><?= $informationTeam['strAlternate']; ?></h2> <!-- nom de l'équipe --->
+                                <img class="imgEcusson" src="<?= $informationTeam['strTeamBadge']; ?>" alt=""> <!-- écusson --->
+                            </div>
+
+                            <div class="created">
+                                <h3>Creation</h3>
+                                <span><?= $informationTeam['intFormedYear']; ?></span> <!-- Année de création --->
+                            </div>
+                            <div class="maillots">
+                                <h3>Nos maillots</h3>
+                                <img class="imgTShirt" src="<?= $informationTeam['strTeamJersey']; ?>" alt=""> <!-- les maillots --->
+                            </div>
                         </div>
-
-                        <div class="created">
-                            <h3>Creation</h3>
-                            <span><?= $informationTeam['intFormedYear']; ?></span> <!-- Année de création --->
-                        </div>
-                        <div class="maillots">
-                            <h3>Nos maillots</h3>
-                            <img class="imgTShirt" src="<?= $informationTeam['strTeamJersey']; ?>" alt=""> <!-- les maillots --->
+                        <div class="faceCache">
+                            <div class="description">
+                                <h3>Description</h3>
+                                <p><?= $informationTeam['strDescriptionEN']; ?></p>
+                            </div><!-- description anglaise --->
+                            <div class="stadium">
+                                <h3><?= $informationTeam['strStadium']; ?></h3> <!-- nom du stade --->
+                                <img class="imgStadium" src="<?= $informationTeam['strStadiumThumb']; ?>" alt=""> <!-- photo du stade --->
+                            </div>
                         </div>
                     </div>
-                    <div class="faceCache">
-                        <div class="description">
-                            <h3>Description</h3>
-                            <p><?= $informationTeam['strDescriptionEN']; ?></p>
-                        </div><!-- description anglaise --->
-                        <div class="stadium">
-                            <h3><?= $informationTeam['strStadium']; ?></h3> <!-- nom du stade --->
-                            <img class="imgStadium" src="<?= $informationTeam['strStadiumThumb']; ?>" alt=""> <!-- photo du stade --->
-                        </div>
-                    </div>
-                </div>
-            </a>
+                </a>
 
 
 
-    <?php   }
-        echo "</main>";
-    }
-    ?>
+        <?php   }
+        }
+        ?>
+    </main>
 
-    <!DOCTYPE html>
-    <html lang="fr">
+</body>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
-        <title>Football</title>
-    </head>
-
-    <body>
-        <nav>
-            <!-- mettre une navbar ici -->
-        </nav>
-    </body>
-
-    </html>
+</html>
